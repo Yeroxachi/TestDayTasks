df17482c8a5d02956798f972396a9e19ac4474fb
Yeroxachi <82865890+Yeroxachi@users.noreply.github.com>
2025-09-16 16:00:40 +0500

    game object added with repo

---
+++ src/back/TileGameEngine/TileGameEngine.Domain/Entities/GameObject.cs
@@ -0,0 +1,41 @@
+﻿using MemoryPack;
+
+namespace TileGameEngine.Domain.Entities;
+
+[MemoryPackable]
+public partial class GameObject
+{
+    public uint Id { get; }
+    public TileCoordinate Position { get; private set; }
+    public int Width { get; }
+    public int Height { get; }
+    public string Type { get; }
+
+    public GameObject(uint id, TileCoordinate position, int width, int height, string type)
+    {
+        Id = id;
+        Position = position;
+        Width = width;
+        Height = height;
+        Type = type ?? throw new ArgumentNullException(nameof(type));
+    }
+
+    public void MoveTo(TileCoordinate newPosition)
+    {
+        Position = newPosition;
+    }
+
+    public Area GetBoundingArea()
+    {
+        return new Area(Position.X, Position.Y,
+            Position.X + Width - 1, Position.Y + Height - 1);
+    }
+
+    public bool IntersectsWith(Area area)
+    {
+        var objectArea = GetBoundingArea();
+
+        return !(objectArea.X2 < area.X1 || objectArea.X1 > area.X2 ||
+                 objectArea.Y2 < area.Y1 || objectArea.Y1 > area.Y2);
+    }
+}
